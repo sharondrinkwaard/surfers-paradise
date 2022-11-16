6a56c0500364bb9d4ebaede0125c3890748a8862
sharondrinkwaard <sharondrinkwaard@gmail.com>
2022-11-16 13:03:51 +0000

    Update models.py; add email field

--- surfclub/models.py
+++ surfclub/models.py
@@ -2,27 +2,29 @@ from django.db import models
 from django.contrib.auth.models import User
 from django import forms
 
-ALL_CHOICES = (
-    ('1', 'Beginner Surf Lesson - No experience'),
-    ('2', 'Intermediate Surf Lesson - 5 > lessons'),
-    ('3', 'Surf Coaching - 2 hours')
-    ('4', 'Surf Coaching - 4 hours')
-    ('5', 'Kids Camp')
-)
+# ALL_CHOICES = (
+#     ('1', 'Beginner Surf Lesson - No experience'),
+#     ('2', 'Intermediate Surf Lesson - 5 > lessons'),
+#     ('3', 'Surf Coaching - 2 hours'),
+#     ('4', 'Surf Coaching - 4 hours'),
+#     ('5', 'Kids Camp'),
+# )
 
 
-class Booking(model.Models):
-    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='#')
+class Bookings(models.Model):
+    customer_name = models.ForeignKey(User, on_delete=models.CASCADE, related_name='#')
+    email = models.EmailField()
     updated_on = models.DateTimeField(auto_now=True)
     created_on = models.DateTimeField(auto_now=True)
+    # booking_date
     # options = forms.MultipleChoiceField(choices=ALL_CHOICES)
     notes = models.TextField(blank=True)
-    # status = approved / not approved
+    status = models.BooleanField(default=False)
     # booking_nr = 
 
     class Meta:
         ordering = ['-created_on']
 
     def __str__(self):
-        return self.customer
+        return f'Booking overview: {self.customer_name, self.email, self.notes}'
 
