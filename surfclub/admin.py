ba3b6341d9d309b5a28eff7ea100f5e9b21bc98b
sharondrinkwaard <sharondrinkwaard@gmail.com>
2022-11-17 11:53:02 +0000

    Add list display and search field to admin page

--- surfclub/admin.py
+++ surfclub/admin.py
@@ -5,4 +5,7 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Booking)
 class BookingAdmin(SummernoteModelAdmin):
+    list_display = ('first_name', 'last_name', 'date', 'time', 'email')
+    search_fields = ['first_name', 'last_name', 'date', 'email']
+    list_filter = ('date',)  # sorts bookings by date
     summernotes_fields = ('content')
