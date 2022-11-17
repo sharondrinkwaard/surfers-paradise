1492243d64e0ae94eb9b3f9320b3a2c365240e8e
sharondrinkwaard <sharondrinkwaard@gmail.com>
2022-11-17 11:36:56 +0000

    Add html files

--- surfclub/admin.py
+++ surfclub/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Booking
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Bookings)
+
+@admin.register(Booking)
+class BookingAdmin(SummernoteModelAdmin):
+    summernotes_fields = ('content')
--- surfclub/models.py
+++ surfclub/models.py
@@ -1,30 +1,44 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 from django import forms
 
-# ALL_CHOICES = (
-#     ('1', 'Beginner Surf Lesson - No experience'),
-#     ('2', 'Intermediate Surf Lesson - 5 > lessons'),
-#     ('3', 'Surf Coaching - 2 hours'),
-#     ('4', 'Surf Coaching - 4 hours'),
-#     ('5', 'Kids Camp'),
-# )
+
+ALL_CHOICES = (
+    ('Beginner Surf Lesson', 'Beginner Surf Lesson'),
+    ('Intermediate Surf Lesson', 'Intermediate Surf Lesson'),
+    ('Surf Coaching', 'Surf Coaching'),
+    ('Kids Camp', 'Kids Camp'),
+)
+
+TIME_CHOICES = (
+    ('10:00', '10:00'),
+    ('12:00', '12:00'),
+    ('14:00', '14:00'),
+    ('16:00', '16:00'),
+    ('18:00', '18:00'),
+)
 
 
-class Bookings(models.Model):
-    customer_name = models.ForeignKey(User, on_delete=models.CASCADE, related_name='#')
+class Booking(models.Model):
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    options = models.CharField(max_length=80, choices=ALL_CHOICES, default='Beginner Surf Lesson')
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
     email = models.EmailField()
     updated_on = models.DateTimeField(auto_now=True)
     created_on = models.DateTimeField(auto_now=True)
-    # booking_date
-    # options = forms.MultipleChoiceField(choices=ALL_CHOICES)
+    date = models.DateField(default=datetime.now)
+    time = models.CharField(max_length=10, choices=TIME_CHOICES, default='10:00')
     notes = models.TextField(blank=True)
-    status = models.BooleanField(default=False)
+    approved = models.BooleanField(default=False)
     # booking_nr = 
 
     class Meta:
         ordering = ['-created_on']
 
     def __str__(self):
-        return f'Booking overview: {self.customer_name, self.email, self.notes}'
+        return f'Name: {self.first_name, self.last_name} | Date: {self.date} | Time {self.time} | Notes: {self.notes}'
+
+
 
