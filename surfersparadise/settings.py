0cbc2e85bf9a2fcc9c9d752cf38110313eeb8386
sharondrinkwaard <sharondrinkwaard@gmail.com>
2022-11-21 23:17:02 +0000

    Delete commented out code + add favicon

--- surfersparadise/settings.py
+++ surfersparadise/settings.py
@@ -163,7 +163,6 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'CLOUDINARY_NAME',
-    # 'API_KEY': 'API_CLOUD',
     'API_SECRET': 'API_SECRET_KEY'
 }
 
